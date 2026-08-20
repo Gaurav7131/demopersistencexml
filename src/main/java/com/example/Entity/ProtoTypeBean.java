0ce8156484a97cafe4ac304de8503dfd34239303
Gaurav7131 <thakaregaurav7131@gmail.com>
2026-08-20 14:33:04 +0530

    Implemented Prototype Bean Instance inside Singleton Scoped with @Lookup  sucessfully

--- src/main/java/com/example/Entity/ProtoTypeBean.java
+++ src/main/java/com/example/Entity/ProtoTypeBean.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class ProtoTypeBean {
-    private final Long instanceId = System.currentTimeMillis();
+    // private final Long instanceId = System.currentTimeMillis();
 
     // method
     public void showId() {
+        // Calculate timestamp inside the method dynamically
+        long instanceId = System.currentTimeMillis();
         System.out.println("Prototype Bean Instance ID:" + instanceId);
     }
 
